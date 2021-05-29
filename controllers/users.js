8656e3c3e3fad1db2b11a9759c8ba9c388c62640
Maria Smickersgill <mariasmickersgill@live.com>
2021-05-29 12:52:30 +0200

    Initial commit for movies app

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+// Display all favourite movies
+exports.getFavourites = (req, res, next)=> {
+    // Some logic to fetch user's faves from database and display them 
+}
+
+// Add movie to my favourites 
+exports.addFavourite = (req, res)=> {
+    userFavourites.push(req.body.whatever); 
+    // Or something 
+}
+
+// Delete movie from favourite
+exports.deleteFromFavourites = (req, res)=> {
+    // presuming that the favourites are stored in some kind of array 
+    // Maybe use a filter function 
+    userFavourites.filter((movie)=> {
+        movie.id !== req.params.id; 
+    })
+}
